e9de5f0085a16739ab444482713426efb7f68ff4
cmy <nuanyang233@gmail.com>
2021-06-21 10:59:17 +0800

    feat: wallet history

--- packages/web3-shared/src/types/index.ts
+++ packages/web3-shared/src/types/index.ts
@@ -256,3 +256,46 @@ export enum PortfolioProvider {
 export enum CollectibleProvider {
     OPENSEAN,
 }
+
+export enum FilterTransactionType {
+    ALL = 'all',
+    SENT = 'sent',
+    RECEIVE = 'receive',
+}
+
+export enum DebankTransactionDirection {
+    SEND = 'send',
+    RECEIVE = 'receive',
+}
+
+export enum ZerionTransactionDirection {
+    IN = 'in',
+    OUT = 'out',
+    SELF = 'self',
+}
+
+export type TransactionDirection = ZerionTransactionDirection | DebankTransactionDirection
+
+export interface TransactionPair {
+    name: string
+    symbol: string
+    address: string
+    direction: TransactionDirection
+    amount: number
+}
+
+export type TransactionGasFee = {
+    eth: number
+    usd: number
+}
+
+export interface Transaction {
+    type: string | undefined
+    id: string
+    timeAt: Date
+    toAddress: string
+    failed: boolean
+    pairs: TransactionPair[]
+    gasFee: TransactionGasFee | undefined
+    transactionType: string
+}
