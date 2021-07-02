8a1fd7d0ed5f048fc8ba7f5febf01bbf2bbe4d05
Septs <github@septs.pw>
2021-07-02 14:39:27 +0800

    fix: build failed

--- packages/constants/compile-constants.ts
+++ packages/constants/compile-constants.ts
@@ -4,7 +4,7 @@ import { run } from '../cli/utils'
 
 const DATA_PATH = path.join(__dirname, 'data')
 
-const chainNames = ['Mainnet', 'Ropsten', 'Rinkeby', 'Kovan', 'Gorli', 'BSC', 'BSCT', 'Matic', 'Mumbai']
+const chainNames = ['Mainnet', 'Ropsten', 'Rinkeby', 'Kovan', 'Gorli', 'BSC', 'BSCT', 'Matic', 'Mumbai', 'ARBITRUM']
 
 async function main() {
     // format files
